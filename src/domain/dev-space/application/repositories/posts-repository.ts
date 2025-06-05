64732d55a1064ee805bf4cbd1b79d487bcb2fde4
DanielVieiraFernandes <danivieiiira@gmail.com>
2025-06-05 12:11:29 -0300

    feat: adicionando novos métodos no contrato do post

--- src/domain/dev-space/application/repositories/posts-repository.ts
+++ src/domain/dev-space/application/repositories/posts-repository.ts
@@ -2,4 +2,6 @@ import { Post } from "@/domain/dev-space/enterprise/entities/post";
 
 export abstract class PostsRepository {
   abstract create(post: Post): Promise<void>;
+  abstract save(post: Post): Promise<void>;
+  abstract findById(id: string): Promise<Post | null>;
 }
